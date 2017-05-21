d610182395439bd2ec35624e1006c7016f9df160
Italo <italowco@gmail.com>
2017-05-21 14:12:38 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import {MdSidenav, MdDialog, MdDialogConfig } from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'AWESOME BANK';
 }
